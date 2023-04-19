c23f5e3ce99fd910607f57734d09b4791119d858
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-04-19 18:50:19 +0530

    added cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from pymongo import MongoClient
 from bson import ObjectId, json_util
 import json
@@ -8,6 +9,8 @@ from sabi_ml import getSimilar
 
 load_dotenv()
 app = Flask(__name__)
+CORS(app, origins=["http://localhost:3000", "https://sabi.onrender.com",
+     "https://sabi-dev.onrender.com", "https://sabi-test.onrender.com"])
 mongoClient = MongoClient(os.getenv("MONGO_URI"))
 db = mongoClient["SABI"]
 
