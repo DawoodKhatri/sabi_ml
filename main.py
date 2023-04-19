f5586f87e89b03cfcc22aac3e0366445dccba40a
DawoodKhatri <dawoodkhatri18@gmail.com>
2023-04-19 17:41:47 +0530

    removed extra imports

--- main.py
+++ main.py
@@ -5,14 +5,9 @@ import json
 import os
 from dotenv import load_dotenv
 from sabi_ml import getSimilar
-import pymongo
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer,CountVectorizer
-from sklearn.metrics.pairwise import linear_kernel
 
 load_dotenv()
 app = Flask(__name__)
-print(os.getenv("MONGO_URI"))
 mongoClient = MongoClient(os.getenv("MONGO_URI"))
 db = mongoClient["SABI"]
 
